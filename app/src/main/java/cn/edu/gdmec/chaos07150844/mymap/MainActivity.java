d03f4c66276a9db39888a8e1d992c9d40263c1a2
gdmec07150844 <daaixiaoliu@qq.com>
2016-12-14 21:08:17 +0800

    new class

--- app/src/main/java/cn/edu/gdmec/chaos07150844/mymap/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/chaos07150844/mymap/MainActivity.java
@@ -221,6 +221,7 @@ public class MainActivity extends AppCompatActivity {
         LocationManager locationManager = (LocationManager) getSystemService(serviceString);
         String provider = LocationManager.GPS_PROVIDER;
         Location location = null;
+
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
